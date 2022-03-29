48da3b4cbd506b2c5599c485bd57db36635c018d
Markus Bartels <m.b.other@live.de>
2022-03-29 08:02:47 +0000

    German translation added.

---
+++ resources/lang/de/ui.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'users' => 'Benutzer',
+    'roles' => 'Rollen',
+    'permissions' => 'Berechtigungen',
+    'save' => 'Speichern',
+    'setup_message' => 'Bitte führe \'php artisan lumki:setup\' via CLI aus',
+
+    'leave_impersonation' => 'Nachahmung verlassen',
+
+    'manage_permissions' => 'Berechtigungen verwalten',
+    'create_permission' => 'Berechtigungen erstellen',
+    'placeholder_permission' => 'Name der Berechtigung',
+
+    'manage_roles' => 'Rollen verwalten',
+    'edit_permissions' => 'Berechtigung bearbeiten',
+    'create_role' => 'Rolle erstellen',
+    'placeholder_role' => 'Name der neuen Rolle',
+
+    'manage_users' => 'Benutzer verwalten',
+    'edit_roles' => 'Rolle bearbeiten',
+    'impersonate' => 'Benutzer wechseln',
+];
